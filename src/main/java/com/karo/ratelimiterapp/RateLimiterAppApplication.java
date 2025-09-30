70b23f01d9868f66c6c6effcba4eebecbbbf1151
Karok365 <anshsinghh365@gmail.com>
2025-09-30 19:33:50 +0530

    Initial

---
+++ src/main/java/com/karo/ratelimiterapp/RateLimiterAppApplication.java
@@ -0,0 +1,13 @@
+package com.karo.ratelimiterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimiterAppApplication.class, args);
+    }
+
+}
